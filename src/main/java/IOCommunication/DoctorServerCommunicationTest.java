bcf871baeef0dd8befa3e8a4f18e497c9c888a8c
nataliagarciasanchez <natalia.garciasanchez@usp.ceu.es>
2024-12-06 19:03:03 +0100

    Doctor Swing

--- src/main/java/IOCommunication/DoctorServerCommunicationTest.java
+++ src/main/java/IOCommunication/DoctorServerCommunicationTest.java
@@ -33,8 +33,8 @@ public class DoctorServerCommunicationTest {
         com.start();
         send= com.new Send();
         role=new Role();
-        //register();
-        login();
+        register();
+        //login();
         //updateInfo();
         //viewPersonalInfo();
         //viewPatients();
